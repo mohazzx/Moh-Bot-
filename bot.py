fbeaeb9b36ee2bbf3a385d1db1fcd32f12e8de4f
mohazzx <39884777+mohazzx@users.noreply.github.com>
2020-02-14 22:10:58 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import urllib.request
 from discord.utils import get
 
 
-bot_token = 'Njc3NzkyMTI2MzQ3NjQwODUy.XkbY5g.x1Wrleo_9q6bWA5JvI3K-iIugJk'
+bot_token = 'Njc3NzkyMTI2MzQ3NjQwODUy.XkbaFw.ljyBTxsw3jfMlM6RdUnUPoUCTBI'
 
 
 
